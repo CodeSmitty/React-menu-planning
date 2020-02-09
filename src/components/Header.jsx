365686466622899c05a8677af3bcc6bbf41c49bd
codesmitty <varcodesmitty@gmail.com>
2020-02-08 23:30:08 -0500

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header(){
+
+return(  <div className='header'>
+    <h1 className='beans' >Beans & Cornbread</h1>
+
+
+  </div>
+)
+
+};
+
+export default Header;
