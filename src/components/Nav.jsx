999453c6fc9dee949a4446d0a85e4f27eb6543fc
codesmitty <varcodesmitty@gmail.com>
2020-02-09 03:22:12 -0500

    experiments

---
+++ src/components/Nav.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+
+
+
+
+
+function Nav(){
+return (
+  <div>
+    <nav >
+      <p className='menuP'>Menu</p>
+      <hr></hr>
+        <ul>
+          <li><button>Create Menu</button></li>
+          <li><button>Archived Menus</button></li>
+          <li><button>Recipe Links</button></li>
+        </ul>
+    </nav>
+  </div>
+
+
+  )
+}
+
+
+export default Nav;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './components/Header'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Nav from './components/Nav';
+import Table from './components/Table';
 
 
 ReactDOM.render(<div>
 
 <Header />
+<Nav />
+<Table />
+<Footer />
 </div>
 , document.getElementById('root'));
